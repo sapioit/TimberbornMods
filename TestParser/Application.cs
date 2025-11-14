d87765448e4188621955b45d52e7d84169571269
Igor Zavoychinskiy <igor.zavoychinskiy@gmail.com>
2025-11-13 18:36:38 -0800

    Test on real game code

--- TestParser/Application.cs
+++ TestParser/Application.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using TestParser.Export;
+using IgorZ.Automation.AutomationSystem;
+using IgorZ.Automation.ScriptingEngine.Parser;
 
 namespace TestParser;
 
@@ -14,7 +15,7 @@ public class Application {
       // "1 * (2 / 3)",
       // "1 * 2 / 3",
       // "(1 * 2) / 3",
-      // "1 % 2 % 3",
+      "1 % 2 % 3",
       // "1 % (2 % 3)",
       // "(1 % 2) % 3",
       // "(1 % 2) / 3",
@@ -25,17 +26,21 @@ public class Application {
       // "max(12, 13, 14)/ (min (1-(2-3),2,3) / Test.Var1) + (Signals.Set(\"yellow1\", 34))",
       // "max(12, 13, 14)/ (min ((1-2)-3),2,3) / Test.Var1)",
       // "100 >= -200",
-      "Inventory.OutputGood.Water == 100 and Inventory.OutputGood.Water == 200 or Inventory.OutputGood.Water == 300",
-      "(Inventory.OutputGood.Water == 100 and Inventory.OutputGood.Water == 200 or Inventory.OutputGood.Water == 300)",
-      "test(Inventory.OutputGood.Water == 100 and Inventory.OutputGood.Water == 200 or Inventory.OutputGood.Water == 300)",
+      // "Inventory.OutputGood.Water == 100 and Inventory.OutputGood.Water == 200 or Inventory.OutputGood.Water == 300",
+      // "(Inventory.OutputGood.Water == 100 and Inventory.OutputGood.Water == 200 or Inventory.OutputGood.Water == 300)",
+      // "test(Inventory.OutputGood.Water == 100 and Inventory.OutputGood.Water == 200 or Inventory.OutputGood.Water == 300)",
+      // "Inventory.OutputGood.Water >= 100 and not (100 == 200 % 23 + 1 or 10 != 10)",
+      //"Inventory.OutputGood.Water >= -100 and not 100 == 200 % 23 + 1 or 10 != 10 or Signals.Set('yellow', 12)",
   ];
 
   void Run() {
     var parser = new PythonSyntaxParser();
+    var behavior = new AutomationBehavior();
     foreach (var testFormula in _testSamples) {
       Console.WriteLine("Test: " + testFormula);
-      var expression = parser.ParseExpression(testFormula);
-      Console.WriteLine("Deconstruct: " + parser.DeconstructPython(expression));
+      var result = parser.Parse(testFormula, behavior);
+      var expression = result.ParsedExpression;
+      Console.WriteLine("Deconstruct: " + parser.Decompile(expression));
       Console.WriteLine(expression);
       Console.WriteLine();
     }
