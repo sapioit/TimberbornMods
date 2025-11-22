d60df6e75f63dce713ed5bb6dc8d3d3354110b42
Igor Zavoychinskiy <igor.zavoychinskiy@gmail.com>
2025-11-21 21:20:16 -0800

    Refactor to GetPropertyFunction. Deprecate later

--- Automation/ScriptingEngine/Expressions/GetPropertyOperator.cs
+++ Automation/ScriptingEngine/Expressions/GetPropertyOperator.cs
@@ -3,158 +3,90 @@
 // License: Public Domain
 
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
 using IgorZ.Automation.ScriptingEngine.Core;
-using IgorZ.Automation.ScriptingEngine.ScriptableComponents.Components;
-using Timberborn.BaseComponentSystem;
 
 namespace IgorZ.Automation.ScriptingEngine.Expressions;
 
-class GetPropertyOperator : AbstractOperator, IValueExpr {
+sealed class GetPropertyOperator : AbstractOperator, IValueExpr {
 
   public enum OpType {
     GetString,
     GetNumber,
   }
 
-  public readonly OpType OperatorType;
-
+  public OpType OperatorType { get; private init; }
   /// <inheritdoc/>
-  public ScriptValue.TypeEnum ValueType { get; }
+  public ScriptValue.TypeEnum ValueType { get; private init; }
   /// <inheritdoc/>
-  public Func<ScriptValue> ValueFn { get; }
+  public Func<ScriptValue> ValueFn { get; private init; }
 
   /// <summary>Tells if this operator accesses a list property.</summary>
-  public bool IsList { get; }
+  public bool IsList { get; private init; }
 
-  public static GetPropertyOperator CreateGetNumber(ExpressionContext context, IList<IExpression> operands) =>
-      new(OpType.GetNumber, context, operands);
-  public static GetPropertyOperator CreateGetString(ExpressionContext context, IList<IExpression> operands) =>
-      new(OpType.GetString, context, operands);
-
-  /// <inheritdoc/>
-  public override string ToString() {
-    return $"{GetType().Name}({OperatorType})";
-  }
-
-  GetPropertyOperator(OpType opType, ExpressionContext context, IList<IExpression> operands) : base(operands) {
-    OperatorType = opType;
-    ValueType = opType switch {
-        OpType.GetNumber => ScriptValue.TypeEnum.Number,
-        OpType.GetString => ScriptValue.TypeEnum.String,
-        _ => throw new ArgumentOutOfRangeException(nameof(opType), opType, null),
-    };
-    AssertNumberOfOperandsRange(1, -1);
-    var fullPropertyName = GetStringLiteral(0);
-    var parts = fullPropertyName.Split('.');
-    if (parts.Length != 2) {
-      throw new ScriptError.ParsingError($"Bad property name: {Operands[0]}");
-    }
-
-    var propValueFn = context.ScriptingService.GetPropertySource(fullPropertyName, context.ScriptHost);
-    if (propValueFn == null) {
-      var componentName = parts[0];
-      var component = GetComponentByName(context.ScriptHost, componentName);
-      if (!component) {
-        throw new ScriptError.BadStateError(context.ScriptHost, $"Component {componentName} not found");
-      }
-      var propertyName = parts[1];
-      var property = component.GetType().GetProperty(propertyName);
-      if (property == null) {
-        throw new ScriptError.ParsingError($"Property not found: '{fullPropertyName}'");
-      }
-      propValueFn = () => property.GetValue(component)
-          ?? (property.PropertyType == typeof(string) ? "NULL" : Activator.CreateInstance(property.PropertyType));
-    }
-
-    var listObject = propValueFn();
-    var listVal = GetAsList(listObject);
-    if (listVal != null) {
-      IsList = true;
-      if (operands.Count == 1) {
-        if (ValueType != ScriptValue.TypeEnum.Number) {
-          throw new ScriptError.ParsingError("The list type counter cannot be accessed as string");
-        }
-        propValueFn = () => GetAsList(listObject).Count;
-      } else {
-        AssertNumberOfOperandsExact(2);
-        if (Operands[1] is not IValueExpr { ValueType: ScriptValue.TypeEnum.Number } indexExpr) {
-          throw new ScriptError.ParsingError($"Second operand must be a numeric value, found: {Operands[1]}");
-        }
-        propValueFn = () => {
-          var list = GetAsList(listObject);
-          var index = indexExpr.ValueFn().AsInt;
-          if (index < 0 || index >= operands.Count) {
-            throw new ScriptError.ValueOutOfRange($"Index {index} is out of range: [{0}; {list.Count})");
-          }
-          return list[index];
-        };
-      }
+  // // FIXME: Deprecate.
+  public static GetPropertyOperator CreateGetNumber(ExpressionContext context, IList<IExpression> operands) {
+    GetPropertyFunction res;
+    if (operands.Count == 2) {
+      res = GetPropertyFunction.CreateGetCollectionElement(
+          context, GetStringLiteral(operands, 0), GetValueExpr(operands, 1));
+    } else if (operands.Count != 1) {
+      throw new ScriptError.ParsingError($"Expected 1 or 2 operands, got {operands.Count}");
     } else {
-      AssertNumberOfOperandsExact(1);
+      try {
+        res = GetPropertyFunction.CreateGetListLength(context, GetStringLiteral(operands, 0));
+      } catch (ScriptError.ParsingError) {
+        res = GetPropertyFunction.CreateGetOrdinary(context, GetStringLiteral(operands, 0));
+      }
     }
+    return Wrap(OpType.GetNumber, res);
+  }
 
-    var propType = propValueFn().GetType();
-    var propTypeName = IsList ? $"List({propType})" : propType.ToString();
-    ValueFn = ValueType switch {
-        ScriptValue.TypeEnum.Number when propType == typeof(int) => () => ScriptValue.FromInt((int)propValueFn()),
-        ScriptValue.TypeEnum.Number when propType == typeof(float) => () => ScriptValue.FromFloat((float)propValueFn()),
-        ScriptValue.TypeEnum.Number when propType == typeof(bool) => () => ScriptValue.FromBool((bool)propValueFn()),
-        ScriptValue.TypeEnum.Number => throw new ScriptError.ParsingError(
-            $"Property {fullPropertyName} is of incompatible type: {propTypeName}"),
-        ScriptValue.TypeEnum.String when propType == typeof(string) => () => ScriptValue.Of((string)propValueFn()),
-        ScriptValue.TypeEnum.String => throw new ScriptError.ParsingError(
-            $"Property {fullPropertyName} is of incompatible type: {propTypeName}"),
-        _ => throw new ArgumentOutOfRangeException(nameof(ValueType), ValueType, null),
+  // FIXME: Deprecate.
+  public static GetPropertyOperator CreateGetString(ExpressionContext context, IList<IExpression> operands) {
+    var res = operands.Count switch {
+        1 => GetPropertyFunction.CreateGetOrdinary(context, GetStringLiteral(operands, 0)),
+        2 => GetPropertyFunction.CreateGetCollectionElement(
+            context, GetStringLiteral(operands, 0), GetValueExpr(operands, 1)),
+        _ => throw new ScriptError.ParsingError($"Expected 1 or 2 operands, got {operands.Count}"),
+    };
+    return Wrap(OpType.GetString, res);
+  }
+
+  static string GetStringLiteral(IList<IExpression> expressions, int index) {
+    if (expressions.Count <= index) {
+      throw new ScriptError.ParsingError($"Expected at least #{index + 1} arguments");
+    }
+    if (expressions[index] is not ConstantValueExpr { ValueType: ScriptValue.TypeEnum.String } strValueExpr) {
+      throw new ScriptError.ParsingError($"Argument #{index + 1} must be a string literal");
+    }
+    return strValueExpr.ValueFn().AsString;
+  }
+
+  static IValueExpr GetValueExpr(IList<IExpression> expressions, int index) {
+    if (expressions.Count <= index) {
+      throw new ScriptError.ParsingError($"Expected at least #{index + 1} arguments");
+    }
+    if (expressions[index] is not IValueExpr valueExpr) {
+      throw new ScriptError.ParsingError($"Argument #{index + 1} must be a value");
+    }
+    return valueExpr;
+  }
+
+  static GetPropertyOperator Wrap(OpType opType, GetPropertyFunction func) {
+    IExpression[] operands = func.IndexExpr == null
+        ? [ConstantValueExpr.CreateStringLiteral(func.PropertyFullName)]
+        : [ConstantValueExpr.CreateStringLiteral(func.PropertyFullName), func.IndexExpr];
+    return new GetPropertyOperator(opType, func.ValueType, func.ValueFn, operands) {
+        IsList = func.FunctionName is GetPropertyFunction.FuncName.Element or GetPropertyFunction.FuncName.Length,
     };
   }
 
-  static BaseComponent GetComponentByName(BaseComponent baseComponent, string name) {
-    if (name == "Inventory") {
-      // Special case: the buildings can have more than one inventory. 
-      return InventoryScriptableComponent.GetInventory(baseComponent, throwIfNotFound: false);
-    }
-    var components = baseComponent.AllComponents.OfType<BaseComponent>();
-    return components.FirstOrDefault(x => x.GetType().Name == name);
+  GetPropertyOperator(
+      OpType opType, ScriptValue.TypeEnum valueType, Func<ScriptValue> valueFn, IList<IExpression> operands)
+      : base(operands) {
+    OperatorType = opType;
+    ValueType = valueType;
+    ValueFn = valueFn;
   }
-
-  /// <summary>Converts an object to a list. The object must implement the GetEnumerator method.</summary>
-  static IList GetAsList(object value) {
-    if (value is string) {
-      return null;  // Strings are enumerable, but they aren't lists.
-    }
-    var getEnumeratorMethod = value.GetType().GetMethod("GetEnumerator", BindingFlags.Public | BindingFlags.Instance);
-    if (getEnumeratorMethod == null) {
-      return null;
-    }
-    var enumerator = getEnumeratorMethod.Invoke(value, null);
-    if (enumerator is not IEnumerator enumeratorObj) {
-      return null;
-    }
-    var list = new List<object>();
-    while (enumeratorObj.MoveNext()) {
-      list.Add(enumeratorObj.Current);
-    }
-    if (list.Count <= 0) {
-      return list;
-    }
-    // The list must contain trivial types, and be sorted (for the repeatable outcome).
-    var sampleValue = list[0];
-    if (GoodTypes.All(x => !x.IsAssignableFrom(sampleValue.GetType()))) {
-      // The list values aren't trivial. We can't handle them.
-      throw new ScriptError.ParsingError($"Unsupported list type: {sampleValue.GetType()}");
-    }
-    list.Sort();
-    return list;
-  }
-
-  static readonly List<Type> GoodTypes = [
-      typeof(string),
-      typeof(int),
-      typeof(float),
-      typeof(bool),
-  ];
 }
