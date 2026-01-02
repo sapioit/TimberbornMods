1c468abb0dc49177a6908fab276c6b0da980f351
Igor Zavoychinskiy <igor.zavoychinskiy@gmail.com>
2026-01-02 01:23:08 -0800

    Implement district resource signals

--- Automation/ScriptingEngine/ScriptableComponents/Components/DistrictScriptableComponent.cs
+++ Automation/ScriptingEngine/ScriptableComponents/Components/DistrictScriptableComponent.cs
@@ -3,25 +3,87 @@
 // License: Public Domain
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using IgorZ.Automation.AutomationSystem;
 using IgorZ.Automation.ScriptingEngine.Core;
 using IgorZ.Automation.ScriptingEngine.Expressions;
-using Timberborn.BaseComponentSystem;
 using Timberborn.Bots;
+using Timberborn.Common;
 using Timberborn.DwellingSystem;
 using Timberborn.GameDistricts;
+using Timberborn.Goods;
+using Timberborn.ResourceCountingSystem;
+using Timberborn.TickSystem;
+using UnityDev.Utils.LogUtilsLite;
 
 namespace IgorZ.Automation.ScriptingEngine.ScriptableComponents.Components;
 
-sealed class DistrictScriptableComponent : ScriptableComponentBase {
+sealed class DistrictScriptableComponent : ScriptableComponentBase, ITickableSingleton, ILateTickable {
 
   const string BotPopulationSignalLocKey = "IgorZ.Automation.Scriptable.District.Signal.Bots";
   const string BeaversPopulationSignalLocKey = "IgorZ.Automation.Scriptable.District.Signal.Beavers";
   const string NumberOfBedsSignalLocKey = "IgorZ.Automation.Scriptable.District.Signal.NumberOfBeds";
+  const string ResourceCapacitySignalLocKey = "IgorZ.Automation.Scriptable.District.Signal.ResourceCapacity";
+  const string ResourceStockSignalLocKey = "IgorZ.Automation.Scriptable.District.Signal.ResourceStock";
 
   const string BotPopulationSignalName = "District.Bots";
   const string BeaverPopulationSignalName = "District.Beavers";
   const string NumberOfBedsSignalName = "District.NumberOfBeds";
+  const string ResourceStockSignalNamePrefix = "District.ResourceStock.";
+  const string ResourceCapacitySignalNamePrefix = "District.ResourceCapacity.";
+
+  #region ITickableSingleton implemenation
+
+  /// <inheritdoc/>
+  public void Tick() {
+    // The cost of this method will raise proportionally to the number of the rules, so keep it cheap. Optimize the code
+    // and build indexes to avoid computations that can be made in the modifying methods and events.
+    if (TrackersByDistrict.Count > 0) {
+      foreach (var pair in TrackersByDistrict) {
+        var resourceCounter = pair.Key.GetComponent<DistrictResourceCounter>();
+        foreach (var tracker in pair.Value) {
+          foreach (var goodId in tracker.GoodCapacity.Keys.ToArray()) { // Need a copy!
+            var value = resourceCounter._capacityCounter.GetInputOutputCapacity(goodId);
+            if (tracker.GoodCapacity[goodId] == value) {
+              continue;
+            }
+            tracker.GoodCapacity[goodId] = value;
+            tracker.ScheduleSignal(ResourceCapacitySignalNamePrefix + goodId, ignoreErrors: true);
+          }
+          foreach (var goodId in tracker.GoodStock.Keys.ToArray()) { // Need a copy!
+            var value = resourceCounter._stockCounter.GetInputOutputStock(goodId)
+                + resourceCounter._stockCounter.GetOutputStock(goodId);
+            if (tracker.GoodStock[goodId] == value) {
+              continue;
+            }
+            tracker.GoodStock[goodId] = value;
+            tracker.ScheduleSignal(ResourceStockSignalNamePrefix + goodId, ignoreErrors: true);
+          }
+        }
+      }
+    }
+    if (TrackersWithNoDistrict.Count > 0) {
+      foreach (var tracker in TrackersWithNoDistrict) {
+        foreach (var goodId in tracker.GoodCapacity.Keys.ToArray()) { // Need a copy!
+          if (tracker.GoodCapacity[goodId] == 0) {
+            continue;
+          }
+          tracker.GoodCapacity[goodId] = 0;
+          tracker.ScheduleSignal(ResourceCapacitySignalNamePrefix + goodId, ignoreErrors: true);
+        }
+        foreach (var goodId in tracker.GoodStock.Keys.ToArray()) { // Need a copy!
+          if (tracker.GoodStock[goodId] == 0) {
+            continue;
+          }
+          tracker.GoodStock[goodId] = 0;
+          tracker.ScheduleSignal(ResourceStockSignalNamePrefix + goodId, ignoreErrors: true);
+        }
+      }
+    }
+  }
+
+  #endregion
 
   #region ScriptableComponentBase implementation
 
@@ -41,6 +103,14 @@ sealed class DistrictScriptableComponent : ScriptableComponentBase {
     if (!districtBuilding) {
       throw new ScriptError.BadStateError(behavior, "Not a district building");
     }
+    if (name.StartsWith(ResourceStockSignalNamePrefix)) {
+      var goodId = ParseResourceSignalName(name).Id;
+      return () => ResourceStockSignal(behavior, goodId);
+    }
+    if (name.StartsWith(ResourceCapacitySignalNamePrefix)) {
+      var goodId = ParseResourceSignalName(name).Id;
+      return () => ResourceCapacitySignal(behavior, goodId);
+    }
     return name switch {
         BeaverPopulationSignalName => () => BeaverPopulationSignal(districtBuilding),
         BotPopulationSignalName => () => BotPopulationSignal(districtBuilding),
@@ -55,6 +125,12 @@ sealed class DistrictScriptableComponent : ScriptableComponentBase {
     if (!districtBuilding) {
       throw new ScriptError.BadStateError(behavior, "Not a district building");
     }
+    if (name.StartsWith(ResourceStockSignalNamePrefix)) {
+      return LookupSignalDef(name, () => MakeResourceStockTrackerSignalDef(name));
+    }
+    if (name.StartsWith(ResourceCapacitySignalNamePrefix)) {
+      return LookupSignalDef(name, () => MakeResourceCapacityTrackerSignalDef(name));
+    }
     return name switch {
         BeaverPopulationSignalName => BeaverPopulationSignalDef,
         BotPopulationSignalName => BotPopulationSignalDef,
@@ -66,15 +142,23 @@ sealed class DistrictScriptableComponent : ScriptableComponentBase {
   /// <inheritdoc/>
   public override void RegisterSignalChangeCallback(SignalOperator signalOperator, ISignalListener host) {
     var name = signalOperator.SignalName;
-    if (name is not (BeaverPopulationSignalName or BotPopulationSignalName or NumberOfBedsSignalName)) {
+    if (name.StartsWith(ResourceStockSignalNamePrefix) || name.StartsWith(ResourceCapacitySignalNamePrefix)
+        || name is BeaverPopulationSignalName or BotPopulationSignalName or NumberOfBedsSignalName) {
+      host.Behavior.GetOrCreate<DistrictChangeTracker>().AddSignal(signalOperator, host);
+    } else {
       throw new InvalidOperationException("Unknown signal: " + name);
     }
-    host.Behavior.GetOrCreate<DistrictChangeTracker>().AddSignal(signalOperator, host);
   }
 
   /// <inheritdoc/>
   public override void UnregisterSignalChangeCallback(SignalOperator signalOperator, ISignalListener host) {
-    host.Behavior.GetOrThrow<DistrictChangeTracker>().RemoveSignal(signalOperator, host);
+    var name = signalOperator.SignalName;
+    if (name.StartsWith(ResourceStockSignalNamePrefix) || name.StartsWith(ResourceCapacitySignalNamePrefix)
+        || name is BeaverPopulationSignalName or BotPopulationSignalName or NumberOfBedsSignalName) {
+      host.Behavior.GetOrThrow<DistrictChangeTracker>().RemoveSignal(signalOperator, host);
+    } else {
+      throw new InvalidOperationException("Unknown signal: " + name);
+    }
   }
 
   #endregion
@@ -111,6 +195,48 @@ sealed class DistrictScriptableComponent : ScriptableComponentBase {
   };
   SignalDef _numberOfBedsSignalDef;
 
+  SignalDef MakeResourceStockTrackerSignalDef(string signalName) {
+    var spec = ParseResourceSignalName(signalName);
+    return new SignalDef {
+        ScriptName = signalName,
+        DisplayName = Loc.T(ResourceStockSignalLocKey, spec.PluralDisplayName.Value),
+        Result = new ValueDef {
+            ValueType = ScriptValue.TypeEnum.Number,
+            ValueValidator = ValueDef.RangeCheckValidatorInt(min: 0),
+        },
+    };
+  }
+
+  SignalDef MakeResourceCapacityTrackerSignalDef(string signalName) {
+    var spec = ParseResourceSignalName(signalName);
+    return new SignalDef {
+        ScriptName = signalName,
+        DisplayName = Loc.T(ResourceCapacitySignalLocKey, spec.PluralDisplayName.Value),
+        Result = new ValueDef {
+            ValueType = ScriptValue.TypeEnum.Number,
+            ValueValidator = ValueDef.RangeCheckValidatorInt(min: 0),
+        },
+    };
+  }
+
+  ScriptValue ResourceStockSignal(AutomationBehavior behavior, string goodId) {
+    var districtCenter = behavior.GetComponent<DistrictBuilding>().District;
+    if (!districtCenter) { // Disconnected buildings don't have District.
+      return ScriptValue.Of(0);
+    }
+    var resourceCounter = districtCenter.GetComponent<DistrictResourceCounter>();
+    return ScriptValue.FromInt(resourceCounter.GetResourceCount(goodId).TotalStock);
+  }
+
+  ScriptValue ResourceCapacitySignal(AutomationBehavior behavior, string goodId) {
+    var districtCenter = behavior.GetComponent<DistrictBuilding>().District;
+    if (!districtCenter) { // Disconnected buildings don't have District.
+      return ScriptValue.Of(0);
+    }
+    var resourceCounter = districtCenter.GetComponent<DistrictResourceCounter>();
+    return ScriptValue.FromInt(resourceCounter.GetResourceCount(goodId).InputOutputCapacity);
+  }
+
   static ScriptValue BeaverPopulationSignal(DistrictBuilding districtBuilding) {
     return ScriptValue.FromInt(districtBuilding.District?.DistrictPopulation.Beavers.Count ?? 0);
   }
@@ -132,10 +258,27 @@ sealed class DistrictScriptableComponent : ScriptableComponentBase {
 
   #region Implementation
 
-  readonly BaseInstantiator _instantiator;
+  readonly IGoodService _goodService;
 
-  DistrictScriptableComponent(BaseInstantiator instantiator) {
-    _instantiator = instantiator;
+  static readonly Dictionary<DistrictCenter, HashSet<DistrictChangeTracker>> TrackersByDistrict = new ();
+  static readonly HashSet<DistrictChangeTracker> TrackersWithNoDistrict = [];
+
+  DistrictScriptableComponent(IGoodService goodService) {
+    _goodService = goodService;
+    TrackersByDistrict.Clear();
+    TrackersWithNoDistrict.Clear();
+  }
+
+  GoodSpec ParseResourceSignalName(string signalName) {
+    var parts = signalName.Split('.', 3);
+    if (parts.Length != 3) { // Callers must check it!
+      throw new InvalidOperationException($"Malformed resource signal name: {signalName}");
+    }
+    var goodSpec = _goodService.GetGoodOrNull(parts[2]);
+    if (goodSpec == null) {
+      throw new ScriptError.ParsingError("Unknown resource name: " + parts[2]);
+    }
+    return goodSpec;
   }
 
   #endregion
@@ -144,7 +287,7 @@ sealed class DistrictScriptableComponent : ScriptableComponentBase {
 
   internal sealed class DistrictChangeTracker : AbstractStatusTracker {
 
-    DistrictCenter _currentDistrictCenter;
+    #region AbstractStatusTracker overrides
 
     /// <inheritdoc/>
     public override void Start() {
@@ -155,8 +298,64 @@ sealed class DistrictScriptableComponent : ScriptableComponentBase {
       UpdateDistrictCenter();
     }
 
+    /// <inheritdoc/>
+    public override void AddSignal(SignalOperator signalOperator, ISignalListener host) {
+      base.AddSignal(signalOperator, host);
+      var signalName = signalOperator.SignalName;
+      var district = AutomationBehavior.GetComponent<DistrictBuilding>().District;
+      var resourceCounter = district?.GetComponentInChildren<DistrictResourceCounter>();
+      if (signalName.StartsWith(ResourceCapacitySignalNamePrefix)) {
+        var goodId = signalName[ResourceCapacitySignalNamePrefix.Length..];
+        if (!GoodCapacity.ContainsKey(goodId)) {
+          var value = resourceCounter != null ? resourceCounter.GetResourceCount(goodId).InputOutputCapacity : 0;
+          GoodCapacity.Add(goodId, value);
+          HostedDebugLog.Fine(AutomationBehavior, "Start tracking district signal: {0}, value={1}", signalName, value);
+        }
+      } else if (signalName.StartsWith(ResourceStockSignalNamePrefix)) {
+        var goodId = signalName[ResourceStockSignalNamePrefix.Length..];
+        if (!GoodStock.ContainsKey(goodId)) {
+          var value = resourceCounter != null ? resourceCounter.GetResourceCount(goodId).TotalStock : 0;
+          GoodStock.Add(goodId, value);
+          HostedDebugLog.Fine(AutomationBehavior, "Start tracking district signal: {0}, value={1}", signalName, value);
+        }
+      }
+    }
+
+    /// <inheritdoc/>
+    public override void RemoveSignal(SignalOperator signalOperator, ISignalListener host) {
+      base.RemoveSignal(signalOperator, host);
+      var signalName = signalOperator.SignalName;
+      if (!signalName.StartsWith(ResourceCapacitySignalNamePrefix)) {
+        return;
+      }
+      var hasAnySuchSignal = ReferenceManager.Signals.SelectMany(x => x.Value).Any(x => x.SignalName == signalName);
+      if (hasAnySuchSignal) {
+        return;  // Still need to track it.
+      }
+      var goodId = signalName[ResourceCapacitySignalNamePrefix.Length..];
+      HostedDebugLog.Fine(AutomationBehavior, "Stop tracking district signal: {0}", signalName);
+      if (!GoodCapacity.Remove(goodId)) { // It's an abnormal situation.
+        throw new InvalidOperationException($"Cannot remove resource capacity for: {signalName}");
+      }
+    }
+
+    #endregion
+
+    #region Implementation
+
+    DistrictCenter _currentDistrictCenter;
+
+    public readonly Dictionary<string, int> GoodCapacity = [];
+    public readonly Dictionary<string, int> GoodStock = [];
+
     void UpdateDistrictCenter() {
       if (_currentDistrictCenter) {
+        if (TrackersByDistrict.TryGetValue(_currentDistrictCenter, out var trackers)) {
+          trackers.Remove(this);
+          if (trackers.Count == 0) {
+            TrackersByDistrict.Remove(_currentDistrictCenter);
+          }
+        }
         _currentDistrictCenter.DistrictPopulation.CitizenAssigned -= OnCitizenAssigned;
         _currentDistrictCenter.DistrictPopulation.CitizenUnassigned -= OnCitizenUnassigned;
         _currentDistrictCenter.DistrictBuildingRegistry.FinishedBuildingRegistered -= FinishedBuildingRegisteredEvent;
@@ -164,10 +363,14 @@ sealed class DistrictScriptableComponent : ScriptableComponentBase {
       }
       _currentDistrictCenter = AutomationBehavior.GetComponent<DistrictBuilding>().District;
       if (_currentDistrictCenter) {
+        TrackersWithNoDistrict.Remove(this);
+        TrackersByDistrict.GetOrAdd(_currentDistrictCenter, () => []).Add(this);
         _currentDistrictCenter.DistrictPopulation.CitizenAssigned += OnCitizenAssigned;
         _currentDistrictCenter.DistrictPopulation.CitizenUnassigned += OnCitizenUnassigned;
         _currentDistrictCenter.DistrictBuildingRegistry.FinishedBuildingRegistered += FinishedBuildingRegisteredEvent;
         _currentDistrictCenter.DistrictBuildingRegistry.FinishedBuildingUnregistered += FinishedBuildingUnregisteredEvent;
+      } else {
+        TrackersWithNoDistrict.Add(this);
       }
     }
 
@@ -200,6 +403,8 @@ sealed class DistrictScriptableComponent : ScriptableComponentBase {
     void FinishedBuildingUnregisteredEvent(object sender, FinishedBuildingUnregisteredEventArgs arg) {
       ScheduleSignal(NumberOfBedsSignalName, ignoreErrors: true);
     }
+
+    #endregion
   }
 
   #endregion
